5e0c4dc56ae7a1eea1c29a47e62e0418c7567e58
LeUnicorn <denisa.halmaghi@ulbsibiu.ro>
2021-01-03 22:20:08 +0200

    test commit

--- JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/login/servlet/jobposting/JobPostings.java
+++ JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/login/servlet/jobposting/JobPostings.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.recruit.jobrecruiting.login.servlet;
+package com.recruit.jobrecruiting.login.servlet.jobposting;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -29,20 +29,6 @@ public class JobPostings extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
     }
