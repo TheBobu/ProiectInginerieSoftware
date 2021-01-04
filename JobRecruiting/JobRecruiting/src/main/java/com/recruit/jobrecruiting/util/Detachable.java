496c4d5ca8d5a4a2806f80475d5333d45411b692
LeUnicorn <denisa.halmaghi@ulbsibiu.ro>
2021-01-04 23:25:49 +0200

    add util

---
+++ JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/util/Detachable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.recruit.jobrecruiting.util;
+
+/**
+ *
+ * @author DENISA
+ */
+public interface Detachable {
+
+    public <T> T detach();
+}
