ffee5f71f824d57f1e2d41671a0335f75724e762
LeUnicorn <denisa.halmaghi@ulbsibiu.ro>
2021-01-05 23:17:31 +0200

    a

--- JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/common/DetachedEntity.java
+++ JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/common/DetachedEntity.java
@@ -3,14 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.recruit.jobrecruiting.util;
+package com.recruit.jobrecruiting.common;
 
 /**
  *
  * @author DENISA
  */
-public interface Detachable {
-
-    public <T> T detach();
-
+public class DetachedEntity {
 }
---
+++ JobRecruiting/JobRecruiting/src/main/java/com/recruit/jobrecruiting/ejb/JobPostBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.recruit.jobrecruiting.ejb;
+
+import com.recruit.jobrecruiting.common.JobPostDetails;
+import com.recruit.jobrecruiting.util.Detachable;
+import com.recruit.jobrecruiting.util.Util;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+
+/**
+ *
+ * @author DENISA
+ */
+@Stateless
+public class JobPostBean {
+
+    private static final Logger LOG = Logger.getLogger(JobPostBean.class.getName());
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<JobPostDetails> getAllJobPosts() {
+        try {
+            Query query = em.createQuery("SELECT j FROM JobPost j");
+            List<Detachable> jobPosts = (List<Detachable>) query.getResultList();
+            return Util.detachEntities(jobPosts);
+        } catch (Exception ex) {
+            throw new EJBException(ex);
+        }
+    }
+
+}
